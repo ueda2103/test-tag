3f856c0bb9a27b0d169e63fbef87fa4068e3fa5d
vagrant <goodmountain37@gmail.com>
2020-08-03 20:55:08 +0000

    [Add] user my page screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  def index
-    
+
+  def index   
   end
 
   def show
     @books = Book.all
     @book = Book.new
     @user = User.find(params[:id])
-
   end
+
 end
