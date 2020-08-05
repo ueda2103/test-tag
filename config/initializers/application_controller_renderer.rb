74e15d5c1ec2c419a10bd5b2cbc31d6cc26b176b
vagrant <goodmountain37@gmail.com>
2020-08-05 09:18:26 +0000

    [Update] sidemenu profile image display function

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '53eb01ed7b1a3bdb8455154df43b66a5831e678d1da367dfa0774896f9fd1bd934c48559ce06d9dff56f7bc46bfbd39ec9278cc5650287e8fe82c67baa7c9947'
