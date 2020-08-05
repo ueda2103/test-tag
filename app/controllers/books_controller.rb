33b22624446f2415f4d18fea9f534df5883c550e
vagrant <goodmountain37@gmail.com>
2020-08-05 17:46:30 +0000

    [Fix] disable other user's posts

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,6 +31,11 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user_id == current_user.id
+    
+    else
+      redirect_to books_path
+    end
   end
 
   def update
