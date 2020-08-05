6465613fcbe3e538660d16fc9f846d001599acad
vagrant <goodmountain37@gmail.com>
2020-08-05 11:52:43 +0000

    [Add] book list page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,28 @@
 class BooksController < ApplicationController
   
   def new
-    
+    @book = Book.new
   end
 
   def create
-    
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
-    
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
     
   end
-  
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_04_023611) do
+ActiveRecord::Schema.define(version: 2020_08_05_114113) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
