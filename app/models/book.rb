7b1458e2067fe9f25e305a75412864ebe645d166
vagrant <goodmountain37@gmail.com>
2020-08-05 16:36:19 +0000

    [Add] error message due to character limit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :user
-  validates :title, :body, presence: true
+  validates :title, presence: true
+  validates :body, presence: true, length:{ maximum: 200 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
   has_many :books, dependent: :destroy
   attachment :image
 end
